87887a574285859e2adfee37627f3cc902be0d45
arienellefsen <arieneellefsen@pantone.com>
2017-11-06 16:41:32 -0500

    Adding reatc files

---
+++ app/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header/Header';
+
+class Main extends React.Component {
+
+    render() {
+        return (
+            <Header  tagline="new app"/>
+        )
+    }
+}
+export default Main;
