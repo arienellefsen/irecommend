44b2a6c08a9d3d91f4c777c396de9622bffe618a
Aladim2017@ <ariene_silva@hotmail.com>
2017-11-07 21:13:17 -0500

    adding login route and login button

---
+++ app/components/Login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = (props) => {
+  return (
+    <header className="top">
+      <h1 className="red">Login</h1>
+      <button type="button" className="btn btn-primary">Login with Facebook</button>
+
+    </header>
+  )
+}
+
+export default Login;
---
+++ app/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = (props) => {
+  return (
+    <header className="top">
+      <h1 className="red">NotFound</h1>
+    </header>
+  )
+}
+
+export default NotFound;
